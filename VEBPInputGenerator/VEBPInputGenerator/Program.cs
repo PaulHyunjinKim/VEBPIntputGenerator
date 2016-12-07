3c3089279c48f487754f2e9019b799ef0be41f7e
Gunyoung kim <Gunyoung kim@DESKTOP-7K9F2HA>
2016-12-07 00:27:44 -0500

    Implemented reader from binary file and checked duplicates

--- VEBPInputGenerator/VEBPInputGenerator/Program.cs
+++ VEBPInputGenerator/VEBPInputGenerator/Program.cs
@@ -11,17 +11,33 @@ namespace VEBPInputGenerator
     {
         static void Main(string[] args)
         {
+
+            HashSet<UInt32> testHash = new HashSet<UInt32>();
+            
             UInt32 vebpInt;
             UInt32 hebpInt;
-            
+            Dictionary<UInt32, HashSet<UInt32>> tempDict = new Dictionary<UInt32, HashSet<UInt32>>();
             using (BinaryReader reader = new BinaryReader(File.Open("VEBPHEBPSet_3.bin",FileMode.Open)))
             {
                 long length = reader.BaseStream.Length;
+                
                 while(reader.BaseStream.Position != length)
                 {
                     vebpInt = reader.ReadUInt32();
                     hebpInt = reader.ReadUInt32();
-                    Console.WriteLine(vebpInt + " " + hebpInt);
+                    //Console.WriteLine(vebpInt + " " + hebpInt);
+                    
+                    if (!tempDict.ContainsKey(vebpInt))
+                    {
+                        HashSet<UInt32> tempHashSet = new HashSet<UInt32>();
+                        tempHashSet.Add(hebpInt);
+                        tempDict.Add(vebpInt, tempHashSet);
+                    }
+                    else
+                    {
+                        bool duplicateCheck = tempDict[vebpInt].Add(hebpInt);
+                        if (!duplicateCheck) Console.WriteLine("there is duplicate!");
+                    }
                 }
                 
             }
